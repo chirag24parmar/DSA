dae73567215a55a7717f7f4ccdd01895c0292112
chirag24parmar <chirag024parmar@gmail.com>
2023-08-16 14:27:38 +0530

    code for searching the element in 2D array

---
+++ Arrays/2D_array.cpp/search_in_2D_array.cpp
@@ -0,0 +1,51 @@
+//WAP to search the element in 2D array
+
+#include<iostream>
+using namespace std;
+
+//function for searching the element
+bool isPresent(int arr[][3], int target, int i, int j){
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (arr [i][j] == target)
+            {
+                return 1;
+            }   
+        }
+    }
+    return 0;
+}
+
+int main()
+{   
+    //declaring 2D array
+    int arr[2][3];
+
+    //taking input
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin>>arr[i][j];
+        }   
+    }
+
+    int target;
+    cout<<"Enter the element to search -: ";
+    cin>>target;
+
+    //checking the element is present or not
+    if (isPresent(arr, target,2 ,3))
+    {
+        cout<<"Found element ";
+    }else{
+        cout<<"Not Found ";
+
+    }
+    
+
+
+    return 0;
+}
