35b2026abb7db7cc4440f44bf7e8f466817cd316
chirag24parmar <chirag024parmar@gmail.com>
2023-10-11 18:20:40 +0530

    Program to find  GCD

---
+++ Arrays/GCD.cpp
@@ -0,0 +1,33 @@
+//WAP to find GREATEST COMMON NUMBER .
+#include<iostream>
+using namespace std;
+
+int gcd(int a,int b){
+    if (a==0)
+        return b;
+    if (b==0)
+        return a;
+
+    while (a != b)
+    {
+        if(a>b){
+            a = a-b;
+        }else{
+            b = b-a;
+        }
+    }
+        
+    return a;
+    
+}
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    cin>>a>>b;
+
+    int ans = gcd(a,b);
+
+    cout<<"GCD of "<<a<<" & "<<b<<" is "<<ans<<endl;
+
+    return 0;
+}
