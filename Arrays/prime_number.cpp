e9ecc7062e601c8f6350f770f4df4266b7ccfde5
chirag24parmar <chirag024parmar@gmail.com>
2023-10-11 13:09:17 +0530

    Program of Prime Number.

---
+++ Arrays/prime_number.cpp
@@ -0,0 +1,35 @@
+//WAP for prime number
+#include<iostream>
+using namespace std;
+
+//function for finding prime number
+int isPrime(int n){
+
+    for (int i =2 ;i<n; i++)
+    {   
+        if (n%i==0)
+        {
+            return false;
+        }
+        
+    }
+    return true;
+    
+} 
+
+//main function
+int main()
+{
+    int n;
+    cin>>n;
+
+    if (isPrime(n)){
+        cout<<"its a prime number"<<endl;
+    }else
+    {
+        cout<<"not a prime number"<<endl;
+    }
+    
+    return 0;
+}
+
