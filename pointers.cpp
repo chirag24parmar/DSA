8bfef898bfd73f6c24ade3c789c52ba21f0adaa8
chirag24parmar <chirag024parmar@gmail.com>
2023-10-22 16:24:44 +0530

    Basic program of pointers.

---
+++ pointers.cpp
@@ -0,0 +1,21 @@
+//simple understanding of pointers
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num = 10;
+    int *ptr = &num;
+
+    cout<<num<<endl;        // print 5
+    cout<<*ptr<<endl;       // print 5
+    cout<<&num<<endl;       // print the address of num
+    cout<<&*ptr<<endl;      // print the adress of num
+    cout<<ptr<<endl;        // print the adress of num
+
+    cout<<"size is - "<<sizeof(num)<<" byte"<<endl; // check the size of pointer or variable
+
+
+
+    return 0;
+}
