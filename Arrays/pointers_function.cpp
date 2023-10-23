5b3276f4656228ed8384a2508d17183e291d8147
chirag24parmar <chirag024parmar@gmail.com>
2023-10-23 19:32:42 +0530

    Pointers function.

---
+++ Arrays/pointers_function.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void printPointer (int *ptr){
+    cout<<"address - "<<ptr<<endl; //print the address
+    cout<<"Print - "<<*ptr<<endl; // print the value
+}
+
+void update (int *ptr){
+
+    ptr = ptr + 1; //updating the address
+    cout<<"address - "<<ptr<<endl;//print the updated address
+    *ptr = *ptr + 1; // updating the value
+    cout<<"print - "<<*ptr<<endl; //print the updated value
+
+}
+
+int main()
+{
+    int value = 5;
+    int *ptr = &value;
+
+    printPointer(ptr);
+    update(ptr);
+    return 0;
+}
