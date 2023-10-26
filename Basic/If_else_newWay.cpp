ecca5897cc43748d49479b04c36f01757a300975
chirag24parmar <chirag024parmar@gmail.com>
2023-10-26 16:36:20 +0530

    new way of if else.

---
+++ Basic/If_else_newWay.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    int a = 4;
+    int b = 3;
+
+    int ans = 0;
+
+    // new way of writing If / else condition
+    ans = (a<b) ? a : b;
+    cout<<ans<<endl;
+
+    a += 2;
+    b += 6;
+
+    ans = (a<b) ? a : b;
+    cout<<ans<<endl;
+
+
+    return 0;
+}
