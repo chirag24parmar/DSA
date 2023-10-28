6a2fcdcfb4efa1b666d1e41558a93827b96f25b1
chirag24parmar <chirag024parmar@gmail.com>
2023-10-28 17:06:10 +0530

    Increase the code quality.

--- Recursion/adding_rangeOfNumber.cpp
+++ Recursion/adding_rangeOfNumber.cpp
@@ -4,22 +4,20 @@ using namespace std;
 
 //recursion function
 int sum(int k){
-    if (k > 0)
-    {
-        return k + sum(k - 1);
-    }
-    else
-    {
+    //Base case
+    if (k == 0)
         return 0;
-    }
-    
+
+    // Recursive relation    
+    return k + sum(k - 1);    
 }
 int main()
 {
     int value;
+    cout<<"Enter the number - ";
     cin>>value;
 
     int result = sum(value);
-    cout<<result<<endl;
+    cout<<"Addition range of "<<value<<" is - "<<result<<endl;
     return 0;
 }
