7dda5e796dab1781c5a854d80cdeb48667fc36cb
chirag24parmar <chirag024parmar@gmail.com>
2023-10-30 17:59:56 +0530

    right angled triangle star pattern.

---
+++ Pattern/Right-Angled_Triangle_Pattern.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+//function for printing right angled pattern
+void rightAngled_pattern(int n){
+    for(int i=1; i<=n; i++){
+        for (int j = 1; j<=i; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number - ";
+    cin>>n;
+
+    rightAngled_pattern(n);
+
+    return 0;
+}
+
+/*
+Input -: 4
+Output -: * 
+          * * 
+          * * *
+          * * * * 
+*/
