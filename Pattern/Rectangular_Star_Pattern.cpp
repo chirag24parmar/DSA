9949ede3320b037246de3792820a08593fb01fc9
chirag24parmar <chirag024parmar@gmail.com>
2023-10-30 17:49:40 +0530

    Rectangular star pattern.

---
+++ Pattern/Rectangular_Star_Pattern.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+//function for printing rectangle pattern
+void rectangle_pattern(int n){
+    for(int i=0; i<n; i++){
+        for (int j = 0; j<n; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number - ";
+    cin>>n;
+
+    rectangle_pattern(n);
+
+    return 0;
+}
