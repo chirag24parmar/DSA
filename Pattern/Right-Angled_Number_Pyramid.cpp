8239d1c8515a5989e10ec5ab7e04deebd3019381
chirag24parmar <chirag024parmar@gmail.com>
2023-10-31 14:11:09 +0530

    Right-Angled Number Pyramid.

---
+++ Pattern/Right-Angled_Number_Pyramid.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+//function for printing the pyramid of number.
+void  number_pyramid(int n){
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;   
+    } 
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number - ";
+    cin>>n;
+
+    number_pyramid(n);
+    return 0;
+}
+
+/*
+Input -: 4
+Output-:
+1
+1 2
+1 2 3
+1 2 3 4
+*/
