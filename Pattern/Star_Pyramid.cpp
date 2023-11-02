0def0c31ef1097103c4e628cf037e91ddb4d63c4
chirag24parmar <chirag024parmar@gmail.com>
2023-11-02 19:20:52 +0530

    Printing Star Pyramid pattern.

---
+++ Pattern/Star_Pyramid.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+//function for printing the star Pyramid
+void starP(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout<<"  ";
+        }
+        for (int k = 0; k < 2*i+1; k++)
+        {
+            cout<<"* ";
+        }
+        for (int p = 0; p < n-i-1; p++)
+        {
+            cout<<"  ";
+        }
+        cout<<endl;
+    } 
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the value - ";
+    cin>>n;
+
+    starP(n);
+    return 0;
+}
+
+/*
+Input Format: N = 3
+Result: 
+  *  
+ *** 
+***** 
+
+*/
