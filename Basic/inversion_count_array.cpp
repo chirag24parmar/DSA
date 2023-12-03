4fb65a04a233e48ba7fce6bb592951e97ca7d7b4
chirag24parmar <chirag024parmar@gmail.com>
2023-12-03 19:21:45 +0530

    inversion count array

---
+++ Basic/inversion_count_array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int inversionArray(int arr[],int n){
+    int count = 0;
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            if(arr[i]>arr[j]){
+                count++;
+            }
+        } 
+    }
+    return count;
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    int arr[] = {1,20,6,4,5};
+    int n = 5;
+    cout<<inversionArray(arr,n);
+
+    return 0;
+}
