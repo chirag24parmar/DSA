97cfa2c4db09d57eb0b147260c791dbf426cd111
chirag24parmar <chirag024parmar@gmail.com>
2024-09-22 15:40:08 +0530

    detecting and removing Loop in Linked List.

--- linked list/find_circular.cpp
+++ linked list/find_circular.cpp
@@ -1,5 +1,6 @@
 //WAP to find it is a circular linked list or not
 #include<iostream>
+#include<map>
 using namespace std;
 
 class Node{
@@ -32,6 +33,7 @@ void insertAtTail(Node* &tail,int d){
     tail = temp;
 }
 
+//this function is for checking the list is circular or not
 bool isCircularList(Node* head){
     //empty list
     if (head == NULL)
@@ -51,6 +53,92 @@ bool isCircularList(Node* head){
         return true;
     }
     return false;
+}
+
+//if the list have loop not circular list
+//this is function is for check that, list contain loop or not
+bool detectloop(Node* head){
+
+    if (head == NULL)
+    {
+        return false;
+    }
+
+    map<Node*,bool> visited;
+    Node* temp = head;
+
+    while (temp != NULL)
+    {
+        if (visited[temp] == true)
+        {
+            return true;
+        }
+        visited[temp] = true;
+        temp = temp -> next;
+    }
+    return false;
+}
+
+//most optimmal solution for detecting loop in list
+Node* floydDetectLoop(Node* head){
+    if (head == NULL)
+    {
+        return NULL;
+    }
+
+    Node* slow = head;
+    Node* fast = head;
+
+    while (slow != NULL && fast != NULL)
+    {
+        fast = fast -> next;
+        if(fast != NULL){
+            fast = fast -> next;
+        }
+
+        slow = slow -> next;
+        if (slow == fast){
+            cout<<" Present at "<< slow -> data <<endl;
+            return slow;
+        }
+    }
+    return NULL;
+}
+
+//this function is for finding the starting node of loop list.
+Node* getStartingNode(Node* head){
+
+    if(head == NULL){
+        return NULL;
+    }
+
+    Node* slow = head;
+    Node* intersection = floydDetectLoop(head);
+
+    while (slow != intersection)
+    {
+        slow = slow -> next;
+        intersection = intersection -> next;
+    }
+    return slow;
+}
+
+//this function is for removing loop from list
+void removeLoop(Node* head){
+    if (head == NULL){
+        return;
+    }
+
+    Node* startOfLoop = getStartingNode(head);
+    Node* temp = startOfLoop;
+
+    while (temp -> next != startOfLoop)
+    {
+        temp = temp -> next;
+    }
+    
+    temp -> next = NULL;
+    
 }
 
 
@@ -77,17 +165,47 @@ int main(){
     Node* tail = node1;
 
     insertAtTail(tail, 12);
+    insertAtTail(tail, 13);
+    insertAtTail(tail, 14);
     insertAtTail(tail, 15);
+    insertAtTail(tail, 16);
+
 
     
     print(head);
 
+    //this is for making loop in list
+    tail -> next = head -> next; 
+    
+    //this for circular
     if(isCircularList(tail)){
-        cout<<"It is a circular Linked list";
+        cout<<"It is a circular Linked list"<<endl;
     }else{
-        cout<<"It is not a circular Linked list";
+        cout<<"It is not a circular Linked list"<<endl;
     };
 
+    // this id detecting loop
+    if (detectloop(head))
+    {
+        cout<<"Loop is present"<< endl;
+    }else{
+        cout<<"No loop is present"<<endl;
+    }
+    
+    // floyd algo for detecting loop
+    if (floydDetectLoop(head) != NULL)
+    {
+        cout<<"Loop is present"<< endl;
+    }else{
+        cout<<"No loop is present"<<endl;
+    }
+    
+    //this for finding starting node of loop list
+    Node* loop = getStartingNode(head);
+    cout<<"loop start at "<<loop -> data<<endl;
+
+    removeLoop(head);
+    print(head);
 
 
 return 0;
--- linked list/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-if (middleNode != NULL) {
-    //     cout << "Middle Node: " << middleNode->data << endl;
-    // } else {
-    //     cout << "List is empty" << endl;
-    // }
